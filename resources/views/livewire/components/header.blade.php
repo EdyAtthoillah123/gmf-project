f6c725ab6cc0bc7d0f6d7a1812543abd1c7f3356
gitfkrid <e41210477@student.polije.ac.id>
2024-06-09 20:16:38 +0700

    dropdown navbar

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -19,15 +19,31 @@
                 <li>
                     <a class="text-title2 text-dgreen hover:font-bold" href="#about">{{ __('messages.about') }}</a>
                 </li>
-                <li>
-                    <a class="text-title2 text-dgreen hover:font-bold" href="#service">{{ __('messages.service') }}</a>
+                <li class="group relative">
+                    <a class="text-title2 text-dgreen hover:font-bold">{{ __('messages.service') }}</a>
+                    <ul
+                        class="hidden absolute top-full left-0 w-48 bg-white shadow-md py-2 rounded-md text-sm text-gray-700 hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out group-hover:block">
+                        <li>
+                            <a class="block px-4 py-2 text-title2 text-dgreen hover:bg-gray-200"
+                                href="#service1">Service 1</a>
+                        </li>
+                        <li>
+                            <a class="block px-4 py-2 text-title2 text-dgreen hover:bg-gray-200"
+                                href="#service2">Service 2</a>
+                        </li>
+                        <li>
+                            <a class="block px-4 py-2 text-title2 text-dgreen hover:bg-gray-200"
+                                href="#service3">Service 3</a>
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <a class="text-title2 text-dgreen hover:font-bold" href="#">{{ __('messages.blog') }}</a>
                 </li>
                 <li class="lg:hidden flex gap-4 items-center">
                     <a href="#" class="text-title2 text-dgreen font-bold px-[10px] py-2">EN</a>
-                    <a href="#" class="text-title2 text-dgreen font-bold border border-dgreen rounded-full px-3 py-2">ID</a>
+                    <a href="#"
+                        class="text-title2 text-dgreen font-bold border border-dgreen rounded-full px-3 py-2">ID</a>
                     {{-- Instagram --}}
                     <a href="" target="_blank"><svg role="img" height="19"
                             class="text-dgreen fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -61,7 +77,8 @@
         </div>
         <div class="flex-1 hidden lg:flex justify-end items-center gap-4">
             <a href="#" class="text-title2 text-dgreen font-bold px-[10px] py-2">EN</a>
-            <a href="#" class="text-title2 text-dgreen font-bold border border-dgreen rounded-full px-3 py-2">ID</a>
+            <a href="#"
+                class="text-title2 text-dgreen font-bold border border-dgreen rounded-full px-3 py-2">ID</a>
             {{-- Facebook --}}
             <a href="" target="_blank"><svg role="img" height="19" class="text-dgreen fill-current px-2"
                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -91,5 +108,4 @@
                         d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg></a>
         </div>
-
 </header>
